3c44bfb9e09239c17d25a895dd16171d06c4616c
tungtk2 <tungtk2.local@tungtran.local>
2023-04-17 21:50:04 +0700

    feat: new cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,7 +31,10 @@ def go(args):
 
     logger.info('Converting last_review column to datetime')
     df['last_review'] = pd.to_datetime(df['last_review'])
-    
+
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info(f'Saving cleaned data to {args.output_artifact}')
     df.to_csv(args.output_artifact, index=False)
 
